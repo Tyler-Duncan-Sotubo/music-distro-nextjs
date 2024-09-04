254318ede6dd89b0cb54160a33afd7bf27634dda
tyler-Wiz <tylertooxclusive@gmail.com>
2024-09-04 22:14:37 +0100

    fix sales view bug

--- src/app/(pages)/dashboard/sales/page.tsx
+++ src/app/(pages)/dashboard/sales/page.tsx
@@ -2,6 +2,7 @@ import RenderSalesPage from "./RenderSalesPage";
 import RenderSalesDemoPage from "./RenderSalesDemoPage";
 import { api } from "@/trpc/server";
 import { type Metadata } from "next";
+import { type Summary } from "./types/sales.types";
 
 export const metadata: Metadata = {
   title: "Sales and Analytics | We Plug Music - Dashboard",
@@ -11,8 +12,8 @@ export const metadata: Metadata = {
 
 const page = async () => {
   const userSubscription = await api.subscriptions.getSubscription();
-  const earnings = await api.report.getTotalEarnings();
-  if (userSubscription?.status === "active" && earnings) {
+  const earnings = (await api.report.getTotalEarnings()) as Summary;
+  if (userSubscription?.status === "active" && earnings.totalEarnings > 0) {
     return <RenderSalesPage />;
   } else {
     return <RenderSalesDemoPage />;
